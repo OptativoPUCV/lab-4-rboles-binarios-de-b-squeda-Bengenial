703b9f54820637a51615fac1a04979d9a5585c5a
Your Name <you@example.com>
2025-04-30 01:17:25 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -37,6 +37,19 @@ TreeNode * createTreeNode(void* key, void * value) {
 }
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
+    TreeMap *map = (TreeMap *) malloc(sizeof(TreeMap));
+    if (map == NULL) return NULL;
+
+    map->root = NULL;
+    map->current = NULL;
+    map->lower_than = lower_than;
+
+
+    return map;
+}
+
+
+void insertTreeMap(TreeMap * tree, void* key, void * value) {
     if (tree->root == NULL) {
         // Si el árbol está vacío, se crea la raíz
         tree->root = createTreeNode(key, value);
